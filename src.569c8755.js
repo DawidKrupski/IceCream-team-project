3f1e62a63fbb32f107126cfcb43958cde71d1405
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2022-09-03 06:55:29 +0000

    Deploying to gh-pages from @ DawidKrupski/IceCream-team-project@ec8dafe7e29ebe74bfceb00591bd356166c15d1c 🚀

--- src.569c8755.js
+++ src.569c8755.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/mobile/advantagesbg.webp":[["advantagesbg.e4cfd87d.webp","rTG0"],"rTG0"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream-team-project/src.26b6e5b8.js.map
+//# sourceMappingURL=/IceCream-team-project/src.569c8755.js.map
